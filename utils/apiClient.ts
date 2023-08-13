b1415919562c31e4045340d83355aaf3521e13e0
niloufars <niloufar.salehi@gmail.com>
2023-08-13 13:53:17 -0700

    fixing api url

--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,7 +1,7 @@
 // utils/apiClient.ts
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : process.env.API_BASE_URL || '' ;
 
 const headers = {
