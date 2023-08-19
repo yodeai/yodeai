80931b358593f060943308329fd888a2f02c9c76
niloufars <niloufar.salehi@gmail.com>
2023-08-18 22:05:37 -0700

    testing whatsapp webhook

--- pages/api/whatsappWebhook.ts
+++ pages/api/whatsappWebhook.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sendWhatsAppMessage } from "./sendWhatsAppMessage";
 
+const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
 
-const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         const mode = req.query['hub.mode'];
         const token = req.query['hub.verify_token'];
@@ -22,38 +22,20 @@ export async function handler(req: NextApiRequest, res: NextApiResponse) {
     } else if (req.method === 'POST') {
         try {
             // Send a test message
-            const testmessage = await sendWhatsAppMessage("16509969470", "Message received!");
-            const testmessage2 = await sendWhatsAppMessage("16509969470", req.body);
+            await sendWhatsAppMessage("16509969470", "Message received!");
+            await sendWhatsAppMessage("16509969470", JSON.stringify(req.body)); 
     
-            /*
-            const incomingMessage = req.body;
             
-            const entry = incomingMessage?.entry?.[0];
-            const change = entry?.changes?.[0];
-    
-            const field = change?.field;
-            const verb = change?.value?.verb;
-            const objectId = change?.value?.object_id;
-            const uid = entry?.uid;
-    
-            // For the sake of this example, I'm assuming senderPhone and receivedText are to be derived from the incomingMessage.
-            // You need to replace this with the correct extraction logic.
-            const senderPhone = entry?.id;  // This is just a placeholder. Replace with correct logic.
-            const receivedText = change?.value;  // Placeholder. Replace with correct logic.
-    
-            if (senderPhone && receivedText) {
-                const responseData = await sendWhatsAppMessage(senderPhone, receivedText);
-                console.log("WhatsApp Message Sent:", { to: senderPhone, message: receivedText, responseData });
-                return res.status(200).json({ message: 'Message echoed successfully.' });
-            } else {
-                console.error("Invalid message format:", incomingMessage);
-                return res.status(400).json({ error: 'Invalid message format.' });
-            }*/
+            return res.status(200).json({ message: 'Test messages sent.' });
+            
+            
         } catch (error) {
-            console.error("Error echoing message:", error);
-            return res.status(500).json({ error: 'Failed to echo message.' });
+            console.error("Error sending message:", error);
+            return res.status(500).json({ error: 'Failed to send message.' });
         }
     } else {
         return res.status(405).json({ error: 'Method not allowed.' });
     }
 }
+
+export default handler; 
