c0edab948d7b5be366a60a5f477ccbdcdfc2cb55
Afshi7n <142748141+Afshi7n@users.noreply.github.com>
2023-09-18 22:06:11 -0700

    nicer scrollable list

---
+++ debug/tools.tsx
@@ -0,0 +1,6 @@
+export  function clearConsole(stuff: any) {
+    console.log("\nbegin--------------\n\n\n\n\n\n\n");
+    console.log(stuff);
+    console.log("\nend--------------\n");
+    return;
+}
