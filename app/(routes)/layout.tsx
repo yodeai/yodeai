6641ce2ed2eb5816326c14f0f4243d41dfc77d58
niloufars <niloufar.salehi@gmail.com>
2023-09-28 23:54:25 -0700

    fixed layout and ordering for narrow screens (e.g. phone).

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,32 +1,22 @@
-
 import Navbar from "@components/Navbar";
 import QuestionAnswerForm from '@components/QuestionAnswerForm'
 import { LensProvider } from "@contexts/lensContext";
 
-export default async function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AppLayout({ children }: { children: React.ReactNode; }) {
 
   return (
-    <>
     <LensProvider>
-      <div className="flex">
-        <div className="overflow-y-auto max-h-[90vh]">
+      <div className="flex flex-col sm:flex-row">
+        <div className="overflow-y-auto order-2 sm:order-1 sm:max-h-[90vh]">
           <Navbar />
         </div>
-        <div className="w-[50vw] overflow-y-auto max-h-[90vh] ">
+        <div className="w-full overflow-y-auto order-3 sm:order-2 sm:w-[50vw] sm:max-h-[90vh]">
           {children}
         </div>
-        <div className="w-[30vw] bg-white border-l  overflow-y-auto max-h-[90vh]">
+        <div className="w-full bg-white border-l overflow-y-auto order-1 sm:order-3 sm:w-[30vw] sm:max-h-[90vh]">
           <QuestionAnswerForm />
         </div>
       </div>
-      </LensProvider>
-    </>
-    
+    </LensProvider>
   );
 }
-
-
