f26c646e3d10cb34beb4aeec155dfbaa78d259f6
niloufars <niloufar.salehi@gmail.com>
2023-10-05 18:01:41 -0700

    added "force-dynamic" whenever using "cookies"

--- app/api/lens/[lens_id]/route.ts
+++ app/api/lens/[lens_id]/route.ts
@@ -2,7 +2,7 @@ import { ok, notOk } from "@lib/ok";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest } from "next/server";
 import { cookies } from 'next/headers';
-
+export const dynamic = 'force-dynamic'
 
 export async function PUT(request: NextRequest, { params,}: {params: { lens_id: string };}) {
     const supabase = createServerComponentClient({ cookies });
