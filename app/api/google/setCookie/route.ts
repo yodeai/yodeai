2e700ed11a6877a18f980992050b7ea5d6c5b464
Dana Feng <danafeng@berkeley.edu>
2024-01-11 22:08:41 -0800

    edit redirect uri

--- app/api/google/setCookie/route.ts
+++ app/api/google/setCookie/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
         code: code,
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: 'http://localhost:3000/auth',
+        redirect_uri: process.env.REDIRECT_URI,
         grant_type: 'authorization_code',
       }
     );
