5431128b2b1c5b66de9b1922f8cdbeaa024f62b6
Samet <hey@samet.codes>
2024-01-27 15:33:42 +0300

    refactor: removed progress imititation

--- app/api/whiteboard/route.ts
+++ app/api/whiteboard/route.ts
@@ -1,58 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from 'next/headers';
 import { notOk, ok } from '@lib/ok';
-import { Database, Tables } from 'app/_types/supabase';
-import { WhiteboardPluginParams } from 'app/_types/whiteboard';
+import { Database } from 'app/_types/supabase';
 
 export const dynamic = 'force-dynamic';
-
-const waitFor = (ms: number) => new Promise(r => setTimeout(r, ms));
-
-const imitateWhiteboardProgress = async (data: Tables<"whiteboard">) => {
-    const supabase = createServerComponentClient<Database>({ cookies });
-
-    // status: queued
-    await supabase.from('whiteboard').update({
-        plugin: {
-            ...data.plugin as WhiteboardPluginParams,
-            state: { status: "queued" }
-        } as WhiteboardPluginParams
-    }).match({ whiteboard_id: data.whiteboard_id });
-
-    // console.log("Whiteboard plugin queued.");
-
-    await waitFor(500);
-
-    // status: processing
-    for (let i = 0; i < 10; i++) {
-        await supabase.from('whiteboard').update({
-            plugin: {
-                ...data.plugin as WhiteboardPluginParams,
-                state: {
-                    status: "processing",
-                    progress: i * 10
-                }
-            } as WhiteboardPluginParams
-        }).match({ whiteboard_id: data.whiteboard_id });
-
-        // console.log("Whiteboard plugin processing.", i);
-
-        await waitFor(100);
-    }
-
-    // status: success
-    await supabase.from('whiteboard').update({
-        plugin: {
-            ...data.plugin as WhiteboardPluginParams,
-            state: {
-                status: "success"
-            }
-        } as WhiteboardPluginParams
-    }).match({ whiteboard_id: data.whiteboard_id });
-
-    // console.log("Whiteboard plugin success.");
-}
 
 export async function POST(request: NextRequest) {
     const supabase = createServerComponentClient<Database>({ cookies });
@@ -81,10 +33,6 @@ export async function POST(request: NextRequest) {
             console.log("error", error.message)
             throw error;
         }
-
-        // if (data.length && plugin) {
-        //     imitateWhiteboardProgress(data[0]);
-        // }
 
         return ok(data);
 
