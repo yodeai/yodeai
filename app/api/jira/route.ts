cc78887592e564f61fe1b98241074ab14fd2439b
Andrew Wang <awang4@berkeley.edu>
2024-01-31 17:22:19 -0800

    replaced atlassian oauth redirect links with my own dev console links

--- app/api/jira/route.ts
+++ app/api/jira/route.ts
@@ -3,9 +3,9 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     if (request.nextUrl.pathname === '/api/jira') {
         if (process.env.NEXT_PUBLIC_API_BASE_URL === "http://localhost:3000/api") {
-            return NextResponse.redirect("https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=VorSFdUOMKYp1BKRWhQLFGWKy5ZWYWBc&scope=read%3Ajira-work%20write%3Ajira-work%20manage%3Ajira-project%20read%3Ajira-user%20manage%3Ajira-configuration%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fjira%2Fcallback&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent");
+            return NextResponse.redirect("https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=sEejUVEYFKYdYGstmzJHkLqy6ajQOJ32&scope=read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20read%3Ajira-user%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fjira%2Fcallback&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent");
         } else {
-            return NextResponse.redirect("https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=ZD8JHgVYBgD2wAwkOzyu0sTMEpEl7A1z&scope=read%3Ajira-work%20manage%3Ajira-configuration%20read%3Ajira-user%20manage%3Ajira-project%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=https%3A%2F%2Fyodeai.vercel.app%2Fapi%2Fjira%2Fcallback&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent");
+            return NextResponse.redirect("https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=o0Y81APO0oYggIbSRzX5ANanH4lDZGPT&scope=read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20read%3Ajira-user%20write%3Ajira-work%20manage%3Ajira-webhook%20manage%3Ajira-data-provider&redirect_uri=https%3A%2F%2Fyodeai.vercel.app%2Fapi%2Fjira%2Fcallback&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent");
         }
     }
 }
