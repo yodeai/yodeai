a0112b0a5423ebdf1e97c26d881f5694cdad9f83
Andrew Wang <awang4@berkeley.edu>
2024-02-12 15:36:30 -0800

    fetching Jira projects

--- app/_components/JiraTicketExportButton.tsx
+++ app/_components/JiraTicketExportButton.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaTicketAlt } from "react-icons/fa";
-import { Button } from "@mantine/core";
+import { Button, Group, Select } from "@mantine/core";
 import { Block } from "app/_types/block";
 
 interface BlockProps {
@@ -9,17 +9,23 @@ interface BlockProps {
 
 const JiraTicketExportButton: React.FC<BlockProps> = ({ block_id }) => {
   const [block, setBlock] = useState<Block | null>(null);
+  const [jiraProjects, setJiraProjects] = useState([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isProjectsLoading, setIsProjectsLoading] = useState<boolean>(true);
+  const [showProjects, setShowProjects] = useState<boolean>(false);
 
-  const handleJiraTicketExport = async () => {
-    setIsLoading(true);
+  // TODO: Implement pagination for Projects API. Use these variables
+  // const [currentProjectsPage, setCurrentProjectsPage] = useState(1);
+  // const [totalProjectsPages, setTotalProjectsPages] = useState(1);
+
+  const handleJiraTicketExport = async (project_id: number) => {
     try {
       const issueBody = JSON.stringify({
         fields: {
           summary: block.title,
           description: block.content,
           project: {
-              key: "YL" // todo: this is hard coded. change to be dynamic
+              id: project_id
           },
           issuetype: {
               id: 10001 // todo: 10001 is "Task" type. see documentation for issue types. https://confluence.atlassian.com/jirakb/finding-the-id-for-issue-types-646186508.html
@@ -40,9 +46,58 @@ const JiraTicketExportButton: React.FC<BlockProps> = ({ block_id }) => {
     } catch (error) {
         console.error("Failed to create Jira issue:", error);
     } finally {
-        setIsLoading(false);
+      setShowProjects(false);
     }
   };
+
+  const handleFetchProjects = async () => {
+    setShowProjects(true);
+    setIsProjectsLoading(true);
+    try {
+      const response = await fetchProjects();
+      if (!response.ok) {
+          throw new Error(response.statusText);
+      }
+    } catch (error) {
+        console.error("Failed to fetch Jira projects:", error);
+    } finally {
+        setIsProjectsLoading(false);
+    }
+  };
+
+  const handleCancelExport = () => {
+    setShowProjects(false);
+  };
+
+  // TODO: Implement pagination for Projects API. Use these handlers
+  // const handlePrevProjectsPage = () => {
+  //   if (currentProjectsPage > 1) {
+  //     setCurrentProjectsPage(currentProjectsPage - 1);
+  //   }
+  // };
+
+  // const handleNextProjectsPage = () => {
+  //   if (currentProjectsPage < totalProjectsPages) {
+  //     setCurrentProjectsPage(currentProjectsPage + 1);
+  //   }
+  // };
+
+  async function fetchProjects() {
+    try {
+        const response = await fetch(`/api/jira/fetchProjects`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch Jira projects');
+        }
+        const data = await response.json();
+        setJiraProjects(data.values.map((elem) => ({
+          value: elem.id,
+          label: elem.name,
+        })) || []);
+        return response;
+    } catch (error) {
+        console.error('Error fetching projects:', error);
+    }
+  }
 
   useEffect(() => {
     fetch(`/api/block/${block_id}`)
@@ -59,12 +114,49 @@ const JiraTicketExportButton: React.FC<BlockProps> = ({ block_id }) => {
       })
   }, [block_id]);
 
+  // TODO: Implement pagination for Projects API. Use this useEffect
+  // useEffect(() => {
+  //   fetchProjects();
+  // }, [currentProjectsPage]);
+
   return (
     <div className="flex gap-2 mt-1 flex-wrap">
-      <Button style={{ height: 24, width: 180 }} size='xs' onClick={handleJiraTicketExport} variant='gradient' gradient={{ from: 'blue.4', to: 'blue.5', deg: 250 }} disabled={isLoading}>
+      {showProjects ? (
+        <Group gap="xs">
+          <Select
+            size="xs"
+            placeholder="Enter Jira Project name..."
+            data={jiraProjects}
+            onOptionSubmit={(value) => handleJiraTicketExport(Number(value))}
+            maxDropdownHeight={150}
+            searchable
+            nothingFoundMessage="None found"
+            disabled={isProjectsLoading}
+          />
+          <Button
+          onClick={handleCancelExport}
+            style={{ height: 24, width: 80 }}
+            size='xs'
+            variant='gradient'
+            gradient={{ from: 'red.4', to: 'red.5', deg: 250 }}
+            disabled={isLoading}
+          >
+            Cancel
+          </Button>
+        </Group>
+      ) : (
+        <Button
+        onClick={handleFetchProjects}
+          style={{ height: 24, width: 180 }}
+          size='xs'
+          variant='gradient'
+          gradient={{ from: 'blue.4', to: 'blue.5', deg: 250 }}
+          disabled={isLoading}
+        >
           <FaTicketAlt size={18} style={{ marginRight: 5 }} />
           {isLoading ? 'Loading...' : "Export as Jira Ticket"}
-      </Button>
+        </Button>
+      )}
     </div>
   )
 };
--- app/api/jira/fetchProjects/route.ts
+++ app/api/jira/fetchProjects/route.ts
@@ -1,8 +1,8 @@
-// pages/api/jira/createIssues.ts
+// pages/api/jira/fetchProjects.ts
 import { NextRequest, NextResponse } from 'next/server';
 import cookie from 'cookie';
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
     const cookies = request.headers.get('cookie');
     const parsedCookies = cookie.parse(cookies || '');
 
@@ -20,33 +20,35 @@ export async function POST(request: NextRequest) {
     try {
         const accessToken = jiraAuthData.accessToken;
         const siteId = jiraAuthData.siteId;
+
         if (!accessToken || !siteId) {
             return new Response("Missing accessToken or siteId", { status: 400 });
         }
 
-        let issueBody = "";
-        for await (const chunk of request.body) {
-            issueBody += Array.from(chunk).map((byte: number) => String.fromCharCode(byte)).join('');
-        }
-
-        // Create a Jira issue
-        const response = await fetch(`https://api.atlassian.com/ex/jira/${siteId}/rest/api/2/issue`, {
-            method: 'POST',
+        // Fetching Jira Projects recursively
+        const response = await fetch(`https://api.atlassian.com/ex/jira/${siteId}/rest/api/2/project/search`, {
+            method: 'GET',
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
                 'Content-Type': 'application/json'
-            },
-            body: issueBody
+            }
         });
 
         // Check for errors in response
         if (!response.ok) {
             const errorResponse = await response.text();
-            console.error('Failed to fetch Jira issues:', errorResponse);
-            return new Response("Error fetching Jira issues", { status: response.status, statusText: errorResponse });
+            console.error('Failed to fetch Jira projects:', errorResponse);
+            return new Response("Error fetching Jira projects", { status: response.status });
         }
 
-        return response;
+        // Return the projects
+        const projects = await response.json();
+        console.log(projects);
+        return new Response(JSON.stringify(projects), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
     } catch (error) {
         console.error('Error:', error);
         return new Response("Server error while fetching Jira issues", { status: 500 });
