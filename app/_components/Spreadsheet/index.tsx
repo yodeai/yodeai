211d958fc0e755542d04d2a7b4c62d473159c743
Samet <hey@samet.codes>
2024-02-14 16:27:09 +0300

    fix: rendering line chart on pain points tracker plugin

--- app/_components/Spreadsheet/index.tsx
+++ app/_components/Spreadsheet/index.tsx
@@ -2,12 +2,10 @@
 
 import { useState, useMemo, useRef, useCallback, useEffect } from 'react';
 
-import { EmitType, registerLicense } from '@syncfusion/ej2-base';
+import { registerLicense } from '@syncfusion/ej2-base';
 import {
     SpreadsheetComponent, SheetsDirective, SheetDirective, ColumnsDirective,
-    getFormatFromType, ChartModel, BeforeCellUpdateArgs,
-    ColumnDirective, RowDirective, RowsDirective,
-    CellsDirective, CellDirective, RangesDirective, RangeDirective
+    ChartModel, ColumnDirective, RangesDirective, RangeDirective
 } from '@syncfusion/ej2-react-spreadsheet';
 import './styles.css';
 import './styles/bootstrap.css';
@@ -15,14 +13,12 @@ import './styles/material3.css';
 
 registerLicense(process.env.NEXT_PUBLIC_SYNCFUSION_LICENSE);
 
-
 import { ImSpinner8 } from "react-icons/im";
 import { Tables } from 'app/_types/supabase';
 import { Text } from '@mantine/core';
 
-import { SpreadsheetCell, SpreadsheetDataSource, SpreadsheetPluginParams } from 'app/_types/spreadsheet';
+import { SpreadsheetDataSource, SpreadsheetPluginParams } from 'app/_types/spreadsheet';
 import { buildDataTable, convertIndexToColumnAlphabet } from './utils'
-import { useDebouncedCallback } from '@utils/hooks';
 
 type SpreadsheetProps = {
     spreadsheet: Tables<"spreadsheet"> & {
@@ -32,27 +28,45 @@ type SpreadsheetProps = {
 }
 
 const Spreadsheet = ({ spreadsheet: { dataSource, plugin, spreadsheet_id } }: SpreadsheetProps) => {
+    const table = buildDataTable(dataSource);
+
     const [cells, setCells] = useState(dataSource);
     const $spreadsheet = useRef<SpreadsheetComponent>()
     const $mounted = useRef<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const chart: ChartModel[] = useMemo(() => {
-        return [{ type: 'Column', range: 'A1:E8' }]
-    }, [cells]);
+    const chart: ChartModel[] = useMemo<ChartModel[]>(() => {
+        return [
+            {
+                type: "Line",
+                theme: "Fabric",
+                range: `A1:${convertIndexToColumnAlphabet(table.columns.length)}${table.records.length + 1}`,
+                name: "Chart",
+                width: table.columns.length * 101,
+                top: ((table.records.length + 1) * 20) + 20,
+                left: 230
+            }
+        ]
+    }, [table]);
 
     const { columns, records } = useMemo(() => buildDataTable(cells), [cells]);
     const isProtected = Boolean(plugin?.name);
 
     const onCreated = useCallback(() => {
         if (columns.length === 0) { return; }
+
         $spreadsheet.current.protectSheet();
-        $spreadsheet.current.hideRibbonTabs(['Home', 'Insert', 'Data', 'View', 'Formulas']);
-        
+        $spreadsheet.current.hideRibbonTabs(['Home', 'Insert', 'Data', 'View', 'Formulas', 'Chart Design']);
+
         $spreadsheet.current.cellFormat({
             backgroundColor: '#e56590', color: '#fff', fontWeight: 'bold', textAlign: 'center'
         }, `A1:${convertIndexToColumnAlphabet(columns.length - 1)}1`);
-        $spreadsheet.current.numberFormat(getFormatFromType('Currency'), `B1:E8`);
+        $spreadsheet.current.cellFormat({
+            backgroundColor: '#ddd', color: '#000', fontWeight: 'bold', textAlign: 'center'
+        }, `A2':A${table.records.length + 1}`);
+        $spreadsheet.current.insertChart(chart);
+        $spreadsheet.current.deselectChart();
+
         $mounted.current = true;
     }, [columns, plugin, isProtected]);
     return (
@@ -73,17 +87,8 @@ const Spreadsheet = ({ spreadsheet: { dataSource, plugin, spreadsheet_id } }: Sp
                                 <RangeDirective dataSource={records} startCell='A1'></RangeDirective>
                             </RangesDirective>
                             {plugin?.state?.status === "success" &&
-                                <RowsDirective>
-                                    <RowDirective index={2}>
-                                        <CellsDirective>
-                                            <CellDirective index={columns.length + 1} chart={chart}></CellDirective>
-                                        </CellsDirective>
-                                    </RowDirective>
-                                </RowsDirective>
-                            }
-                            {plugin?.state?.status === "success" &&
                                 <ColumnsDirective>
-                                    {Array.from({ length: columns.length }, (_, i) => <ColumnDirective width={75} key={i} />)}
+                                    {Array.from({ length: columns.length }, (_, i) => <ColumnDirective width={i === 0 ? 250 : 100} key={i} />)}
                                 </ColumnsDirective>}
                         </SheetDirective>
                     </SheetsDirective>
