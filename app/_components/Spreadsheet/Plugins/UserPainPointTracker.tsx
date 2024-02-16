925c5b63eaff22b01dedbd2150b275ba4fd9cd9b
Samet <hey@samet.codes>
2024-02-16 09:53:44 +0300

    feat: added markers on pain point chart

--- app/_components/Spreadsheet/Plugins/UserPainPointTracker.tsx
+++ app/_components/Spreadsheet/Plugins/UserPainPointTracker.tsx
@@ -36,7 +36,16 @@ export default function ({ $spreadsheet, spreadsheet }: PluginInput): PluginOutp
             range: `A1:${convertIndexToColumnAlphabet(dataTable.columns.length-1)}${dataTable.records.length + 1}`,
             width: dataTable.columns.length * 101,
             top: ((dataTable.records.length + 1) * 20) + 20,
-            left: 230
+            left: 230,
+            markerSettings: {
+                shape: "Circle",
+                size: 5,
+                visible: true,
+                border: {
+                    width: 4
+                }
+            }
+            
         }]
     }, [dataTable]);
 
