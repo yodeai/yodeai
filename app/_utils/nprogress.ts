1bdbe21b1eed6ce8c56127520bfce03228b96f8e
Samet <hey@samet.codes>
2024-02-16 12:27:12 +0300

    fix: added done function for nprogress

--- app/_utils/nprogress.ts
+++ app/_utils/nprogress.ts
@@ -8,6 +8,12 @@ export const useProgressRouter = () => {
     router.push = (href, options) => {
         NProgress.start();
         push(href, options);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                NProgress.done();
+                resolve(true);
+            }, 1500);
+        });
     };
 
     return router;
