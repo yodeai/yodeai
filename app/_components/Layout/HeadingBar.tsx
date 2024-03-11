22c157b889b1103cf1607cce52950196e8ed5d7f
Samet <hey@samet.codes>
2024-03-11 14:47:28 +0300

    fix: showed burger menu only if user session exists

--- app/_components/Layout/HeadingBar.tsx
+++ app/_components/Layout/HeadingBar.tsx
@@ -14,15 +14,16 @@ export default function HeadingBar() {
 
     return (
         <AppShell.Header className="flex items-center align-middle">
-            <Burger
-                opened={openedNavbar}
-                onClick={() => {
-                    toolbarActions.close();
-                    navbarActions.toggle();
-                }}
-                hiddenFrom="sm"
-                size="sm"
-            />
+            {user &&
+                <Burger
+                    opened={openedNavbar}
+                    onClick={() => {
+                        toolbarActions.close();
+                        navbarActions.toggle();
+                    }}
+                    hiddenFrom="sm"
+                    size="sm"
+                />}
             <nav className="flex justify-between items-center w-full bg-white p-[12px] h-[60px] border-b border-b-[#eeeeee]">
                 <Link href="/">
                     <Image src="/yodeai.png" h={32} alt="yodeai logo" />
