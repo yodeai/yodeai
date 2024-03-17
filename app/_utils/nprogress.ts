ece0f1a4b62c46afea8db93910d83a96e2dde755
Samet <hey@samet.codes>
2024-03-17 18:48:31 +0300

    feat: closed navbar and toolbars if user visits a different page

--- app/_utils/nprogress.ts
+++ app/_utils/nprogress.ts
@@ -1,13 +1,26 @@
+import { useAppContext } from "@contexts/context";
+import { useMediaQuery } from "@mantine/hooks";
 import { useRouter } from "next/navigation";
 import NProgress from "nprogress";
 
 export const useProgressRouter = () => {
+    const matchMobileView = useMediaQuery("(max-width: 768px)");
+    const {
+        navbarDisclosure: [openedNavbar, navbarActions],
+        toolbarDisclosure: [openedToolbar, toolbarActions],
+        user
+    } = useAppContext();
+
     const router = useRouter();
     const { push } = router;
 
     router.push = (href, options) => {
         NProgress.start();
         push(href, options);
+        if (matchMobileView) {
+            navbarActions.close();
+            toolbarActions.close();
+        }
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 NProgress.done();
