2d0c18139c7b307005f4062263c7d629834ce98f
Samet <hey@samet.codes>
2024-03-18 21:44:56 +0300

    dep: removed nprogress, added @mantine/nprogress dependency

--- app/_hooks/useProgressRouter.ts
+++ app/_hooks/useProgressRouter.ts
@@ -1,23 +1,19 @@
 import { useAppContext } from "@contexts/context";
 import { useMediaQuery } from "@mantine/hooks";
 import { useRouter } from "next/navigation";
-import NProgress from "nprogress";
 import { getPagePathVersion } from '../_utils/localStorage';
 
 export const useProgressRouter = () => {
     const matchMobileView = useMediaQuery("(max-width: 768px)");
     const {
         navbarDisclosure: [openedNavbar, navbarActions],
-        toolbarDisclosure: [openedToolbar, toolbarActions],
-        user
+        toolbarDisclosure: [openedToolbar, toolbarActions]
     } = useAppContext();
 
     const router = useRouter();
     const { push } = router;
 
     router.push = (href, options) => {
-        NProgress.start();
-
         const getPagePathVersionValue = getPagePathVersion(href);
         href = getPagePathVersionValue ? `${href}?v=${getPagePathVersionValue}` : href;
 
@@ -26,12 +22,6 @@ export const useProgressRouter = () => {
             navbarActions.close();
             toolbarActions.close();
         }
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                NProgress.done();
-                resolve(true);
-            }, 1500);
-        });
     };
 
     return router;
