e6674130cf2c0c80eb35bc908ef8513b52bdeab6
Samet <hey@samet.codes>
2024-03-29 18:52:19 +0300

    refactor: moved user credentials to environment variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'cypress'
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
+  env: {
+    TEST_USER_LOGIN: process.env.TEST_USER_LOGIN,
+    TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD
+  },
   e2e: {
     setupNodeEvents(on, config) { },
     baseUrl: "http://localhost:3000"
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,18 @@
 describe("Login", () => {
+  it("User credential exists", () => {
+    if(!Cypress.env('TEST_USER_LOGIN') || !Cypress.env('TEST_USER_PASSWORD')) {
+      throw new Error('Please provide user credentials in cypress.json')
+    }
+  });
+
   it("User login, and logout", () => {
     cy.viewport(1680, 489);
     cy.visit("http://localhost:3000/landing");
     cy.get('[data-cy="submit"]').click()
 
     cy.get("header span > span").click();
-    cy.get('[data-cy="email"]').focus().type("hey@samet.codes");
-    cy.get('[data-cy="password"]').type("jqufkMDnAt2mI0e");
+    cy.get('[data-cy="email"]').focus().type(Cypress.env('TEST_USER_LOGIN'));
+    cy.get('[data-cy="password"]').type(Cypress.env('TEST_USER_PASSWORD'));
     cy.get('form [data-cy="submit"]').click();
     cy.location("href").should("eq", "http://localhost:3000/");
 
@@ -14,5 +20,5 @@ describe("Login", () => {
     cy.get('[data-cy="logout"]').click();
 
     cy.location("href").should("eq", "http://localhost:3000/login");
-  })
+  });
 });
